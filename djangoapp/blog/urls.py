f115fe010335ef7b94391921b4b0edd5a591e07b
Seu nome <seu_email@gmail.com>
2023-07-08 10:29:51 +0100

    Blog layout

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from blog.views import index
+from blog.views import index, post, page
+
 
 # namespace
 app_name = 'blog'
 urlpatterns = [
     path('', index, name='index'),
+    path('post/', post, name='post'),
+    path('page/', page, name='page'),
 ]
