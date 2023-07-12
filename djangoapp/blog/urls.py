7a0af8dadb7b1e4c236bdfaebd12cec3c8081ab6
Seu nome <seu_email@gmail.com>
2023-07-12 10:58:58 +0100

    get_absolute_url

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -6,6 +6,6 @@ from blog.views import index, post, page
 app_name = 'blog'
 urlpatterns = [
     path('', index, name='index'),
-    path('post/', post, name='post'),
+    path('post/<slug:slug>/', post, name='post'),
     path('page/', page, name='page'),
 ]
