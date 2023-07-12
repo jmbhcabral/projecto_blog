5cea70a26c4a2f7ba338846a03ce3a9a8f50b9cc
jmbhcabral <larbak.cstrike@gmail.com>
2023-07-12 18:39:03 +0100

    Post internal part

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -21,7 +21,44 @@ def index(request):
     )
 
 
-def page(request):
+def created_by(request, author_pk):
+    posts = Post.objects.get_published()\
+        .filter(created_by__pk=author_pk)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+    # O underline underline é a busca pelo foreignkey do post
+    # o slug é um campo da category
+
+
+def category(request, slug):
+    posts = Post.objects.get_published()\
+        .filter(category__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def page(request, slug):
     return render(
         request,
         'blog/pages/page.html',
@@ -31,11 +68,18 @@ def page(request):
     )
 
 
-def post(request):
+def post(request, slug):
+    post = (
+        Post
+        .objects
+        .get_published()
+        .filter(slug=slug)
+        .first()
+    )
     return render(
         request,
         'blog/pages/post.html',
         {
-            # 'page_obj': page_obj,
+            'post': post,
         }
     )
