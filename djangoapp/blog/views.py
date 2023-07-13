97d999beda96cda99579dfba226e4beee43972d9
Seu nome <seu_email@gmail.com>
2023-07-13 10:16:24 +0100

    Tags / search / free page model conclusion

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post, Page
+from django.db.models import Q
 
 PER_PAGE = 9
 
@@ -58,12 +59,59 @@ def category(request, slug):
     )
 
 
+def tag(request, slug):
+    posts = Post.objects.get_published()\
+        .filter(tags__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def search(request):
+    # search = nome do input em post.html
+    search_value = request.GET.get('search', '').strip()
+
+    posts = (
+        Post.objects.get_published()
+        .filter(
+            Q(title__icontains=search_value) |
+            Q(excerpt__icontains=search_value) |
+            Q(content__icontains=search_value)
+        )[:PER_PAGE]
+    )
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value,
+        }
+    )
+
+
 def page(request, slug):
+    page = (
+        Page
+        .objects
+        .filter(is_published=True)
+        .filter(slug=slug)
+        .first()
+    )
     return render(
         request,
         'blog/pages/page.html',
         {
-            # 'page_obj': page_obj,
+            'page': page,
         }
     )
 
