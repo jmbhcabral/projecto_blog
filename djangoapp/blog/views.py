f0c65da9a8830c95f7fd9fb998fdfe7110c83df2
Seu nome <seu_email@gmail.com>
2023-07-13 11:00:55 +0100

    Page titles and http404 for not found

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -2,6 +2,8 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from blog.models import Post, Page
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.http import Http404
 
 PER_PAGE = 9
 
@@ -18,13 +20,24 @@ def index(request):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': 'Home - ',
         }
     )
 
 
 def created_by(request, author_pk):
+    user = User.objects.filter(pk=author_pk).first()
+
+    if user is None:
+        raise Http404
+
     posts = Post.objects.get_published()\
         .filter(created_by__pk=author_pk)
+    user_full_name = user.username
+
+    if user.first_name:
+        user_full_name = f'{user.first_name} {user.last_name}'
+    page_title = 'Posts de ' + user_full_name + ' - '
 
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
@@ -35,6 +48,7 @@ def created_by(request, author_pk):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': page_title,
         }
     )
 
@@ -50,11 +64,16 @@ def category(request, slug):
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
+    if len(page_obj) == 0:
+        raise Http404
+
+    page_title = f'{page_obj[0].category.name} - Categoria - '
     return render(
         request,
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': page_title,
         }
     )
 
@@ -67,11 +86,17 @@ def tag(request, slug):
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
+    if len(page_obj) == 0:
+        raise Http404
+
+    page_title = f'{page_obj[0].tags.first().name} - Tag - '
+
     return render(
         request,
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': page_title,
         }
     )
 
@@ -89,12 +114,15 @@ def search(request):
         )[:PER_PAGE]
     )
 
+    page_title = f'{search_value[:30]} - Search - '
+
     return render(
         request,
         'blog/pages/index.html',
         {
             'page_obj': posts,
             'search_value': search_value,
+            'page_title': page_title,
         }
     )
 
@@ -112,6 +140,7 @@ def page(request, slug):
         'blog/pages/page.html',
         {
             'page': page,
+            'page_title': 'Home - ',
         }
     )
 
@@ -129,5 +158,6 @@ def post(request, slug):
         'blog/pages/post.html',
         {
             'post': post,
+            'page_title': 'Home - ',
         }
     )
